bfd76dbffaee0a62999fbf993d1b20cbc38ccdf2
Rayne <rayneleroux2@gmail.com>
2026-03-11 13:38:45 +0200

    Pass all tests for palindromes exercise.

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const cleanstr = str.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    const reversed = cleanstr.split('').reverse().join('');
+    return cleanstr === reversed;
 };
 
 // Do not edit below this line
