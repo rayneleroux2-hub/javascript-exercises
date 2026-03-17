d5dbc480131508fcd313dbdbb940d7b78acee93a
Rayne <rayneleroux2@gmail.com>
2026-03-17 13:19:11 +0200

    Add 16x16 grid with hover-to-draw-effect.

---
+++ etch-a-sketch/etchASketch.js
@@ -0,0 +1,15 @@
+const container = document.querySelector("#container")
+
+for (let i = 0; i < 256; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square")
+    container.appendChild(square);
+}
+
+const squares = document.querySelectorAll(".square");
+    squares.forEach(square => {
+        square.addEventListener("mouseenter", () => {
+          square.classList.add("active");  
+        });
+    });
+
