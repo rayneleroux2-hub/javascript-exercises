49612c181db3d0deb524a5e059dd9e33cab6a324
Rayne <rayneleroux2@gmail.com>
2026-03-17 22:18:20 +0200

    Add dynamic grid resizing with reset button and hover drawing

--- etch-a-sketch/etchASketch.js
+++ etch-a-sketch/etchASketch.js
@@ -6,10 +6,41 @@ for (let i = 0; i < 256; i++) {
     container.appendChild(square);
 }
 
-const squares = document.querySelectorAll(".square");
+let squares = document.querySelectorAll(".square");
+    squares.forEach(square => {
+        square.addEventListener("mouseenter", () => {
+          square.classList.add("active");  
+        });
+      });
+
+const button = document.querySelector("#reset-grid");
+     button.addEventListener("click", () => {
+      let num = parseInt(prompt("Enter number of squares per side(max 100)"));
+      
+      if (isNaN(num) || num < 1) {
+        num = 16;
+      } else if (num > 100) {
+        num = 100;
+      }
+
+      container.innerHTML = "";
+
+      const squareSize = 100 / num;
+
+for (let i = 0; i< num * num; i++) {
+  const square = document.createElement("div");
+  square.classList.add("square")
+  square.style.width = `${squareSize}%`;
+  square.style.aspectRatio = "1 / 1"; 
+  container.appendChild(square)
+}
+
+ squares = document.querySelectorAll(".square");
     squares.forEach(square => {
         square.addEventListener("mouseenter", () => {
           square.classList.add("active");  
         });
     });
+      
+     })    
 
